4af31f64732696570317de017e5e0f301ef1da09
Jessica Au <aujk@usc.edu>
2020-05-20 14:31:31 -0700

    First commit lol

--- src/index.js
+++ src/index.js
@@ -1,13 +1,124 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import css from './index.scss';
 import App from './App';
+import backButton from "./BackArrow.png"
 import * as serviceWorker from './serviceWorker';
+import { Redirect, Link, BrowserRouter } from 'react-router-dom';
+
+function TopBar(props) {
+  
+  return (
+    <div className="topBar">
+      <Link to='/game'>
+        <img src={backButton} alt="Back"/>
+      </Link>
+      You're playing Truth or Dare in <b>Flirty</b> mode.
+    </div>
+  );
+}
+
+function TruthOrDareButton(props) {
+  return (
+    <button onClick={props.onClick} className={props.className}>
+      {props.choice}
+    </button>
+  );
+}
+
+function GameBox(props) {
+  return (
+    <div className="gameBox">
+      <p>{props.statement}</p>
+    </div>
+  );
+}
+
+class GamePage extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      statement: "Your Truth or Dare will appear here.",
+      selectedOption: "Truth or Dare"
+    };
+  }
+
+  onTruthOrDareClick(tF) {
+    console.log("TF BUTTON CLICKED");
+
+    if (tF === "truth") {
+      this.setState({
+        statement: "OOF YOU CHOSE A TRUTH",
+        selectedOption: "Truth"
+      });
+    }
+    else if (tF === "dare") {
+      this.setState({
+        statement: "OH YEA BUDDY A DARE",
+        selectedOption: "Dare"
+      });
+    }
+  }
+
+  onBackClick() {
+    console.log("BACK BUTTON CLICKED");
+      this.setState({
+        statement: "BACK BUTTON CLICKED",
+        selectedOption: "Dare"
+      });
+    this.setState({redirect: true});
+  }
+
+  render() {
+
+    if (this.state.redirect) {
+      return <Redirect push to="/index.js" />;
+    }
+
+    const statement = this.state.statement;
+    const selectedOption = this.state.selectedOption;
+
+    return (
+      <div className ="main">
+        <TopBar
+          onClick={() => this.onBackClick()}
+        />
+        <div className="optionsContainer">
+          Select Truth or Dare:
+          <br></br>
+          <TruthOrDareButton 
+            onClick={() => this.onTruthOrDareClick("truth")}
+            choice="Truth"
+            className="truthButton"
+          />
+          <br></br>
+          <TruthOrDareButton 
+            onClick={() => this.onTruthOrDareClick("dare")}
+            choice="Dare"
+            className="dareButton"
+          />
+          <br></br>
+        </div>
+        <div className="gameContainer">
+          Your <b>{selectedOption}:</b>
+          <br></br>
+          <GameBox
+            statement={statement}
+          />
+        </div>
+      </div>
+    );
+  }
+
+
+
+
+}
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <BrowserRouter>
+    <GamePage />
+  </BrowserRouter>,
   document.getElementById('root')
 );
 
